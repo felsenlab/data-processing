1aa1eb083d7982505da1d69ce05041dc0bbd0feb
jbhunt <hunt.brian.joshua@gmail.com>
2025-03-17 15:38:35 -0600

    Working on main script

--- process_session.sh
+++ process_session.sh
@@ -6,8 +6,17 @@ if [ $# -lt 1 ]; then
     exit 1
 fi
 
-# Store the first positional argument
+# Assign the home folder to a variable
 home_folder=$1
+
+# Define logging function
+log_file="${home_folder%/}/log.txt"
+log() {
+    local level=$1
+    shift
+    local message="[$(date '+%Y-%m-%d, %H:%M:%S')] $level: $*"
+    echo "$message" | tee -a "$log_file"
+}
 
 # Initialize conda
 source /home/polegpolskylab/anaconda3/etc/profile.d/conda.sh
@@ -16,11 +25,13 @@ source /home/polegpolskylab/anaconda3/etc/profile.d/conda.sh
 cd /home/polegpolskylab/Code/data-processing
 
 # Extract pose estimates
+log "INFO" "Extracting pose estimates"
 conda activate rtdlc
 python ./scripts/extract_pose.py "$home_folder"
 conda deactivate
 
 # Extract real saccades
+log "INFO" "Extracting real saccades"
 conda activate se
 python ./scripts/extract_saccades.py "$home_folder"
 conda deactivate
